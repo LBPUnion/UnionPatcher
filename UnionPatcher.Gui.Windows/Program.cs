d0d075eb8be6f2fa82b3667f6df43cb838c90419
jvyden <jvyden@jvyden.xyz>
2021-10-18 11:23:29 -0400

    Fix for windows

--- UnionPatcher.Gui.Windows/Program.cs
+++ UnionPatcher.Gui.Windows/Program.cs
@@ -1,5 +1,8 @@
-﻿namespace UnionPatcher.Gui.Windows {
+﻿using System;
+
+namespace UnionPatcher.Gui.Windows {
     public static class Program {
+        [STAThread]
         public static void Main() {
             Gui.Show();
         }
