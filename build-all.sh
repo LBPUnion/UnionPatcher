3e2ccda94d8c5ba80b328176db2d7650f918000f
Henry Asbridge <henry@vorax.org>
2024-01-17 23:30:53 +0000

    Created a script for building on mac, as creating a universal binary is somewhat involved

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,4 @@
-mkdir -p builds;
+mkdir -p builds
 #dotnet clean;
 
 dotnet publish -c Windows -r win-x64 --self-contained
@@ -13,19 +13,19 @@ dotnet publish -c MacOS -r osx-arm64 --self-contained
 # $1: Name.zip
 # $2: Path to zip
 function createBuild() {
-    currentDirectory=$(pwd)
-    cd $2 || return 1;
-    
-    zip -r "$1" *;
-    cd $currentDirectory || return 1;
-    mv "$2/$1" builds/
+	currentDirectory=$(pwd)
+	cd $2 || return 1
+
+	zip -r "$1" *
+	cd $currentDirectory || return 1
+	mv "$2/$1" builds/
 }
 
-createBuild "UnionPatcher-Windows-x64.zip" "UnionPatcher.Gui.Windows/bin/Release/net6.0-windows/win-x64/publish/"
+createBuild "UnionPatcher-Windows-x64.zip" "UnionPatcher.Gui.Windows/bin/Release/net8.0-windows/win-x64/publish/"
 
-createBuild "UnionPatcher-Linux-x64.zip" "UnionPatcher.Gui.Linux/bin/Release/net6.0/linux-x64/publish/"
-createBuild "UnionPatcher-Linux-arm.zip" "UnionPatcher.Gui.Linux/bin/Release/net6.0/linux-arm/publish/"
-createBuild "UnionPatcher-Linux-arm64.zip" "UnionPatcher.Gui.Linux/bin/Release/net6.0/linux-arm64/publish/"
+createBuild "UnionPatcher-Linux-x64.zip" "UnionPatcher.Gui.Linux/bin/Release/net8.0/linux-x64/publish/"
+createBuild "UnionPatcher-Linux-arm.zip" "UnionPatcher.Gui.Linux/bin/Release/net8.0/linux-arm/publish/"
+createBuild "UnionPatcher-Linux-arm64.zip" "UnionPatcher.Gui.Linux/bin/Release/net8.0/linux-arm64/publish/"
 
-createBuild "UnionPatcher-macOS-x64.zip" "UnionPatcher.Gui.MacOS/bin/Release/net6.0/osx-x64/publish/"
-createBuild "UnionPatcher-macOS-arm64.zip" "UnionPatcher.Gui.MacOS/bin/Release/net6.0/osx-arm64/publish/"
+# CODESIGN_IDENTITY is the certificate that you want to use for codesigning for mac, if not present then will not be signed
+./build-mac.sh $CODESIGN_IDENTITY
